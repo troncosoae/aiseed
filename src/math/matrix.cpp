1571256eba52ba2db2715b50dccfe9b407c1764a
troncosoae <troncosoae@gmail.com>
2022-06-09 13:57:42 -0400

    adding debug possibility to makfile

--- src/math/matrix.cpp
+++ src/math/matrix.cpp
@@ -9,6 +9,37 @@
 namespace Math
 {
 
+std::string Vect::str()
+{
+	std::stringstream ss;
+	for (uint32_t i=0; i<mLength; i++)
+	{
+		if (i==0) ss << "{";
+		else ss << "\n ";
+		ss << mVector[i];
+	}
+	ss << "}";
+
+	return ss.str();
+}
+
+int Vect::operator*(Vect& vect)
+{
+	//TODO: use math exceptions
+	assert(std::get<0>(vect.shape()) == mLength);
+
+	int output;
+
+	for (uint32_t i=0; i<mLength; ++i)
+	{
+		output += mVector[i]*vect[i];
+	}
+	
+	return output;
+}
+
+
+
 std::string Matrix2D::str()
 {
 	std::stringstream ss;
@@ -43,37 +74,6 @@ Vect Matrix2D::operator*(Vect& vect)
 	
 	return output;
 }
-
-
-std::string Vect::str()
-{
-	std::stringstream ss;
-	for (uint32_t i=0; i<mLength; i++)
-	{
-		if (i==0) ss << "{";
-		else ss << "\n ";
-		ss << mVector[i];
-	}
-	ss << "}";
-
-	return ss.str();
-}
-
-int Vect::operator*(Vect& vect)
-{
-	//TODO: use math exceptions
-	assert(std::get<0>(vect.shape()) == mLength);
-
-	int output;
-
-	for (uint32_t i=0; i<mLength; ++i)
-	{
-		output += mVector[i]*vect[i];
-	}
-	
-	return output;
-}
-
 
 
 } //Math
